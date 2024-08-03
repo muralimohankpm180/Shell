5d0a2a53607b0896beceb65a88e8fb1101d26d46
muralimohankpm180 <muralikpm180@gmail.com>
2024-08-03 08:37:13 +0530

    Initial Commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,36 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+
+
+export const routes: Routes = [
+    {
+        path:'home',
+        component:HomeComponent
+    },
+    //{
+        // path:'dashboard',
+        // loadChildren:()=>{
+        //    return loadRemoteModule({
+        //     type:"module",
+        //     remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        //      exposedModule: './Routes'
+        //    }).then(m=>m.routes)
+        // }
+    //},
+    // {
+    //     path:'login',
+    //     loadComponent:()=>{
+    //        return loadRemoteModule({
+    //         type:"module",
+    //         remoteEntry: 'http://localhost:4201/remoteEntry.js',
+    //          exposedModule: './Login'
+    //        }).then(m=>m.LoginComponent)
+    //     }
+    // },
+    {
+        path:'login',
+        loadComponent: () => 
+            import('remote/Login').then(m => m.LoginComponent)
+    }
+];
