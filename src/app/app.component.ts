1f8d6b8cd8cc0cd8d10d4eeb930157a9e3d10151
Murali Mohan <muralikpm180@gmail.com>
2024-08-05 00:20:19 +0530

    MF with session storage and cutsom events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Shell';
+  cart:any
+  
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+    fromEvent<CustomEvent>(window,'cartEvent').subscribe((customEvent)=>{
+      // this.cart=customEvent.detail
+      console.log(customEvent.detail.cart);
+      this.cart=customEvent.detail.cart
+    })
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  userName = '';
+  ShowUser() {
+    this.userName =
+      sessionStorage.getItem('userid') ?? 'Please login first else some issue';
+  }
 }
